d1e04d7cce710df798ca876efe13d1df5a91a25f
alan zhang <usalanzhang@gmail.com>
2025-05-30 14:32:48 -0700

    demo

---
+++ app/main_agent/agent.py
@@ -0,0 +1,27 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.adk.agents import Agent
+
+root_agent = Agent(
+   # A unique name for the agent.
+   name="main_agent",
+   # The Large Language Model (LLM) that agent will use.
+   model="gemini-2.0-flash-exp",
+   # model="gemini-2.0-flash-live-001",  # New streaming model version as of Feb 2025
+   # A short description of the agent's purpose.
+   description="Agent to answer questions",
+   # Instructions to set the agent's behavior.
+   instruction="You are a helpful assistant that answers user's questions",
+)
