598d4d938623261de629c2ef67ba90be883aaf37
taireyune2 <tyler.wu@keypointvision.com>
2025-06-07 09:46:30 -0700

    intro phase complete

--- backend/python/interviewer/agent.py
+++ backend/python/interviewer/agent.py
@@ -1,116 +1,64 @@
+import logging
+from typing_extensions import override
+from typing import AsyncGenerator, Optional
+
 from google.adk.agents import (
     BaseAgent, LlmAgent, SequentialAgent, ParallelAgent
-) 
+)
 from google.adk.agents.invocation_context import InvocationContext
 from google.adk.agents.callback_context import CallbackContext
 from google.adk.events import Event
 from google.genai import types
-from typing import AsyncGenerator, Optional
-from typing_extensions import override
-import logging  
 
-from .subagents import ontopic_detector, followup_questioner, question_judge
-from .utils import log_agent_context, log_before_model_context, log_after_model_context
-
-sequential_question_formulator = SequentialAgent(
-    name="sequential_question_formulator",
-    sub_agents=[followup_questioner.agent, question_judge.agent],
-    description="Sequential agent that handles follow-up questions and judges responses.",
-)
+from .subagents import  greeter, introducer, overviewer
 
 
-def populate_state(callback_context: CallbackContext) -> Optional[types.Content]:
+class InterviewerAgent(BaseAgent):
     """
-    Store user input into session state before running the agent.
+    Agent that deterministically directs flow to the defined agents
+    based on state using state machine pattern.
+    This agent does not call the LLM, but routes requests to other agents
+    based on predefined rules.
     """
-    if not callback_context.user_content or not callback_context.user_content.parts:
-        return types.Content(role="system", parts=[types.Part(text="User input is required.")])
-
-    # print(f"Running before agent callback.\n{callback_context.user_content.parts[0].text}")
-    callback_context.state["user_response"] = callback_context.user_content.parts[0].text
-    return None
-    # logging.info("Running before agent callback...")
-
-
-root_agent = ParallelAgent(
-    name="root_agent",
-    sub_agents=[ontopic_detector.agent, sequential_question_formulator],
-    description="Parallel agent that detects topics and manages follow-up questions.",
-    before_agent_callback=[log_agent_context, populate_state],
-    after_agent_callback=[log_agent_context],
-)
-
-# instructions = """
-# You are an interviewer.
-
-# You are tasked with asking follow-up questions
-# """
-# root_agent = LlmAgent(
-#     name="root_agent", 
-#     description="Interviewer agent that asks follow-up questions after user response.",
-#     model="gemini-2.0-flash",
-#     instruction=instructions,
-#     tools=[],
-# )
-
-# ### agent that runs a deterministic responses, do not call llm
-# class InterviewerAgent(BaseAgent):
-#     """
-#     Agent that deterministically directs flow to the defined agents
-#     based on state using state machine pattern.
-#     This agent does not call the LLM, but routes requests to other agents
-#     based on predefined rules.
-#     """
-#     ontopic_detector: LlmAgent
-#     followup_questioner: LlmAgent
-#     question_judge: LlmAgent
-#     sequential_agent: SequentialAgent
-#     parallel_agent: ParallelAgent
+    introducer: LlmAgent
+    greeter: LlmAgent
+    overviewer: LlmAgent
     
-#     def __init__(
-#         self, 
-#         ontopic_detector: LlmAgent,
-#         followup_questioner: LlmAgent,
-#         question_judge: LlmAgent,
-#         name: str = "router_agent",
-#     ):
-#         logging.info("Initializing RouterAgent...")
-#         """Initialize the RouterAgent."""
-#         sequential_agent = SequentialAgent(
-#             name="sequential_agent",
-#             sub_agents=[followup_questioner, question_judge],
-#             description="Sequential agent that handles follow-up questions and judges responses.",
-#         )
-#         parallel_agent = ParallelAgent(
-#             name="parallel_agent",
-#             sub_agents=[ontopic_detector, sequential_agent],
-#             description="Parallel agent that detects topics and manages follow-up questions.",
-#         )
-#         super().__init__(
-#             ontopic_detector=ontopic_detector,
-#             followup_questioner=followup_questioner,
-#             question_judge=question_judge,
-#             sequential_agent=sequential_agent,
-#             parallel_agent=parallel_agent,
-#             name=name,
-#             sub_agents=[parallel_agent],
-#         )
+    def __init__(
+        self, 
+        greeter: LlmAgent,
+        introducer: LlmAgent,
+        overviewer: LlmAgent,
+        name: str = "interviewer",
+    ):
+        super().__init__(
+            greeter=greeter,
+            introducer=introducer,
+            overviewer=overviewer,
+            name=name,
+            sub_agents=[greeter, introducer, overviewer],
+            description="Route agents based on the interview phase.",
+        )
 
-#     @override
-#     async def _run_async_impl(self, ctx: InvocationContext) -> AsyncGenerator[Event, None]:
-#         """
-#         Deterministic control flow for other interview agents
-#         """
-#         logging.info("Router agent started#############")
-#         async for event in self.parallel_agent.run_async(ctx):
-#             yield event
-
-      
+    @override
+    async def _run_async_impl(self, ctx: InvocationContext) -> AsyncGenerator[Event, None]:
+        """
+        Deterministic control flow for other interview agents
+        """
+        if ctx.session.state["phase"] == "greeting":
+            async for event in self.greeter.run_async(ctx):
+                yield event
+        if ctx.session.state["phase"] == "introduction":
+            async for event in self.introducer.run_async(ctx):
+                yield event
+        if ctx.session.state["phase"] == "overview":
+            async for event in self.overviewer.run_async(ctx):
+                yield event
 
 
-# root_agent = InterviewerAgent(
-#     name="root_agent",
-#     ontopic_detector=ontopic_detector,
-#     followup_questioner=followup_questioner,
-#     question_judge=question_judge,
-# )
+root_agent = InterviewerAgent(
+    greeter.agent,
+    introducer.agent,
+    overviewer.agent,
+    name="root_agent"
+)
---
+++ backend/python/interviewer/agent_behavior_test.py
@@ -0,0 +1,62 @@
+import pytest 
+import logging
+import json
+import os
+from dotenv import load_dotenv
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.genai import types
+
+from . import agent
+from common import logger
+
+TEST_FOLDER = "../tests/"
+
+def setup():
+    load_dotenv() 
+    with open(os.path.join(TEST_FOLDER, "configs/agent-testing.json"), "r") as f:
+        _configs = json.load(f)
+        configs = _configs["agent"]
+        logger.setup(_configs["logging"])
+
+    session_service = InMemorySessionService()
+    runner = Runner(
+        agent=agent.root_agent,
+        app_name=configs["name"],
+        session_service=session_service,
+    )
+    return configs, session_service, runner
+
+
+async def test_introduction():
+    configs, session_service, runner = setup()
+    with open(os.path.join(TEST_FOLDER, "inputs/intro/expected.json"), "r") as f:
+        data = json.load(f)
+
+    user_id = "123"
+    session_id = user_id
+
+    session = await session_service.create_session(
+        app_name=configs["name"],
+        user_id=user_id,
+        session_id=session_id,
+        state=data["initial_state"],
+    )
+
+    results = []
+    messages = ["", *data["inputs"]]
+    for message in messages:
+        async for event in runner.run_async(
+            user_id=user_id,
+            session_id=session_id,
+            new_message=types.Content(
+                role="user",
+                parts=[types.Part(text=message)]
+            )
+        ):
+            if event.is_final_response():
+                results.append(event.content.parts[0].text)
+
+    for user, agent in zip(messages, results):
+        logging.info(f"User: {user}")
+        logging.info(f"Agent: {agent}")
