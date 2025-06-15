b7d2f163662431a7d0a0eaa8dee981150eee1a0b
taireyune2 <tyler.wu@keypointvision.com>
2025-06-14 21:09:44 -0700

    upload test successful

--- frontend/bRun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/workspace/owlgorithm-hacks/backend
-docker run -it --rm \
-  --name owlspeak-backend-dev -p 8000:8000 \
-  --network owlspeak-dev \
-  --env-file .env \
-  owlspeak/backend:latest 
